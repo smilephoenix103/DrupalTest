ef538e0169b3cd45be5a1834c64e65d386c61ce8
SmilePhoenix <104746557+smilephoenix103@users.noreply.github.com>
2022-10-14 20:05:03 -0400

    Build custom block

---
+++ core/modules/first_module/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\first_module\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ *   category = @Translation("Hello World"),
+ * )
+ */
+class HelloBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#markup' => $this->t('Hello, World!. Kenji Tanaka'),
+    ];
+  }
+
+}
